3f188344160fe476e5886298a1b6b821f94cb0ad
AnilP. <anl.pirincoglu@gmail.com>
2024-01-08 03:56:37 +0300

    add redux logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,17 @@
-import { legacy_createStore as createStore, combineReducers } from "redux";
+import {
+  legacy_createStore as createStore,
+  combineReducers,
+  applyMiddleware,
+} from "redux";
 import movieReducer from "./reducers/movieReducer";
 import favoriteReducer from "./reducers/favoriteReducer";
+import logger from "redux-logger";
 
 const rootReducer = combineReducers({
   movieReducer,
   favoriteReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 export default store;
